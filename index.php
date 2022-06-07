5f86f0ecb303a52e7b36e1cff8cc92886d2f495c
Kylian1208 <kyliancontactepro@gmail.com>
2022-06-07 13:33:28 +0200

    bdd+connexion

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+	<?php include 'bdd/connexion_bdd.php'; ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-	<link rel="stylesheet" href="animate.min.css">
-	<link rel="stylesheet" href="bootstrap.min.css">
-	<link rel="stylesheet" href="font-awesome.min.css">
-	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700,800' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="jquery.fullPage.css">
+    <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Exam 2.0</title>
 </head>
 <body>
@@ -17,7 +14,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<h2 class="wow bounce">JSP</h2>
+					<h2 class="wow bounce">VIVE LA FRANCE</h2>
 				</div>
 				<div class="col-md-4 col-xs-6 wow fadeIn" data-wow-delay="0.6s">
 					<div class="portfolio-thumb">
@@ -49,16 +46,6 @@
 							</div>
 					</div>
 				</div>
-				<div class="col-md-4 col-xs-6 wow fadeIn" data-wow-delay="0.6s">
-					<div class="portfolio-thumb">
-						<img src="images/portfolio-img4.jpg" class="img-responsive" alt="portfolio img">
-							<div class="portfolio-overlay">
-								<h4>Titre Projets</h4>
-								<h5>Info projets</h5>
-								<a href="#"><h3>Voir plus</h3></a>
-							</div>
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
@@ -70,4 +57,5 @@
 		<!-- text rotater -->
 		<script src="js/jquery.simple-text-rotator.js"></script>
 </body>
-</html>
+</html>*
+
